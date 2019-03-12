576861e8740181bfc8b192e303fdee55ae457290
stevenocean <sharexun@163.com>
2019-03-12 16:35:05 +0800

    Fixed only lowercase when encode hex to string.

--- app/src/main/java/io/github/stevenocean/cardemulatorsample/HostCardEmulatorService.java
+++ app/src/main/java/io/github/stevenocean/cardemulatorsample/HostCardEmulatorService.java
@@ -78,8 +78,8 @@ public class HostCardEmulatorService extends HostApduService {
         int len = s.length();
         byte[] data = new byte[len / 2];
         for (int i = 0; i < len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                    + Character.digit(s.charAt(i+1), 16));
+            data[i / 2] = (byte) ((Character.digit(Character.toLowerCase(s.charAt(i)), 16) << 4)
+                    + Character.digit(Character.toLowerCase(s.charAt(i+1)), 16));
         }
         return data;
     }
